3cafe675499ba6c663d1ddc8ecd034b6ec8b3d19
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-09-01 15:43:18 +0800

    add share module to app

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from './pipes/translate.pipe';
+import { TranslateService } from './services/translate.service';
 
 
 
 @NgModule({
-  declarations: [TranslatePipe],
+  declarations: [
+    TranslatePipe
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [
+    TranslateService
+  ],
 })
 export class SharedModule { }
