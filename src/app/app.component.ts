54d7dc5cdbf193912efceed22719f1d9a861336e
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-09-01 16:07:10 +0800

    add select language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from './shared/services/translate.service';
+import { fromEvent, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('language', {static: true}) language$: ElementRef;
+  unsubscribe$ = new Subject();
+
   constructor(private translate: TranslateService){
     this.translate.use('en');
+  }
+
+  ngOnInit() {
+    fromEvent(this.language$.nativeElement, 'change').pipe( 
+      map( (event:Event) => event.target['value']),
+      takeUntil(this.unsubscribe$)
+    ).subscribe( lang => {
+      this.translate.use(lang);
+    })
+  }
+
+  ngOnDestroy(){
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
