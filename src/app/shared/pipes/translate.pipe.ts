7830abf7bfbecb71a8b32d4a73ff7f191db1e3ea
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-09-01 15:45:54 +0800

    implement TranslatePipe

--- src/app/shared/pipes/translate.pipe.ts
+++ src/app/shared/pipes/translate.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from 'src/app/shared/services/translate.service';
 
 @Pipe({
-  name: 'translate'
+  name: 'translate',
+  pure: false
 })
 export class TranslatePipe implements PipeTransform {
+  constructor(private translate: TranslateService) {}
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(key: any): any {
+    return this.translate.translateObject[key] || key;
   }
-
 }
